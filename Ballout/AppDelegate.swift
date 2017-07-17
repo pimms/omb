5bfc2b5249e34103b368561b1b686feecb8e79c1
Joakim Stien <joakim.stien@gmail.com>
2017-07-17 12:15:06 +0200

    Probably fixed sound issues (mix with others)

--- Ballout/AppDelegate.swift
+++ Ballout/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        try? AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback, with: .mixWithOthers)
+        try? AVAudioSession.sharedInstance().setMode(AVAudioSessionModeDefault)
+        try? AVAudioSession.sharedInstance().setActive(true)
+        
         return true
     }
 
